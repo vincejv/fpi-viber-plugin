ed98cef0bcd96b249dc58b172c5b194567a89d8f
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-11-14 02:03:42 +0800

    feat(models): add response dto

---
+++ fpi-viber-plugin-lib/src/main/java/com/abavilla/fpi/viber/ext/dto/SendResponse.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+ * FPI Application - Abavilla                                            *
+ * Copyright (C) 2022  Vince Jerald Villamora                            *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.*
+ *************************************************************************/
+
+package com.abavilla.fpi.viber.ext.dto;
+
+import com.abavilla.fpi.fw.dto.AbsDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@RegisterForReflection
+public class SendResponse extends AbsDto {
+  private Integer status;
+  @JsonProperty("status_message")
+  private String statusMsg;
+  @JsonProperty("message_token")
+  private String msgToken;
+  @JsonProperty("chat_hostname")
+  private String chatHostname;
+  @JsonProperty("billing_status")
+  private Integer billingStatus;
+}
