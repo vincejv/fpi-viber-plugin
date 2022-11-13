1eceda87c3ee50d01d2324d70150098646c112bb
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-11-14 01:14:45 +0800

    feat(viber): add models
    BREAKING CHANGE: bump to v1

---
+++ fpi-viber-plugin-lib/src/main/java/com/abavilla/fpi/viber/ext/dto/ViberUpdate.java
@@ -0,0 +1,39 @@
+/*************************************************************************
+ * Copyright (C) 2022 Vince Jerald Villamora @ https://vincejv.com       *
+ *                                                                       *
+ * Licensed under the Apache License, Version 2.0 (the "License");       *
+ * you may not use this file except in compliance with the License.      *
+ * You may obtain a copy of the License at                               *
+ *                                                                       *
+ *   http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                       *
+ * Unless required by applicable law or agreed to in writing, software   *
+ * distributed under the License is distributed on an "AS IS" BASIS,     *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or       *
+ * implied. See the License for the specific language governing          *
+ * permissions and limitations under the License.                        *
+ *************************************************************************/
+
+package com.abavilla.fpi.viber.ext.dto;
+
+import com.abavilla.fpi.fw.dto.AbsDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@RegisterForReflection
+public class ViberUpdate extends AbsDto {
+  private String event;
+  private Long timestamp;
+  @JsonProperty("chat_hostname")
+  private String chatHostname;
+  private Long messageToken;
+  private Boolean silent;
+}
